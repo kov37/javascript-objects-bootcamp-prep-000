b2c2535442af32e1a9d4400143993cc04b842e2e
kovner chalumo <kchalumo@gmail.com>
2018-12-25 23:07:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { john: "Love", james: "Hate"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist = Object.assign({}, playlist, {[artistName]: songTitle})
   return playlist
 }
 
